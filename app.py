4439a1071f94d0e2e67091b3cdc56cbcc27fdfd5
Brian Brown <brianbrown2002@gmail.com>
2022-12-29 22:19:44 -0500

    changed playlist name

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ def makeplaylist():
         songlist.append("spotify:track:"+i['id'])
     splitsonglist= list(divide_chunks(songlist, 100))
     
-    results = (sp.user_playlist_create(userid, "Repeat Receipts", public=False, collaborative=False, description="The songs you've loved for multiple years."))
+    results = (sp.user_playlist_create(userid, "Repeat Beats", public=False, collaborative=False, description="The songs you've loved for multiple years."))
     for i in splitsonglist:
         sp.user_playlist_add_tracks(userid, results['id'], i)
     return results['external_urls']['spotify']
